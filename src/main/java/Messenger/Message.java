b4e4323fecf11f159b97a64c04f91494b138b1fa
grickodana1-debug <griczko.danya@mail.ru>
2025-11-28 00:15:20 +0300

    проба пера

--- src/main/java/Messenger/Message.java
+++ src/main/java/Messenger/Message.java
@@ -1,4 +1,19 @@
 package Messenger;
 
 public class Message {
+    public String text;
+    public String data;
+    public User fromWho;
+    public Group group;
+    public Channel channel;
+    public User userchat;
+    private int MAX_MESSAGE_SIZE = 5000;
+    private int currentMessagesSize;
+
+    public Message(String text, String data, User fromWho) {
+        this.text = text;
+        this.data = data;
+        this.fromWho = fromWho;
+
+    }
 }
