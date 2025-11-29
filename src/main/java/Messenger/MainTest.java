c80d6f86ed82a31adf39a1f155c38231516bedf0
Igor <0605675@mail.ru>
2025-11-29 09:14:47 +0300

    Исправление ошибок

--- src/main/java/Messenger/MainTest.java
+++ src/main/java/Messenger/MainTest.java
@@ -1,6 +1,6 @@
 package Messenger;
 
-public class Main {
+public class MainTest {
     public static void main(String[] args) {
 
         User Anya = new User("Anya", "@anya");  //легендарные создатели grushinMax
@@ -19,33 +19,34 @@ public class Main {
         Channel channel = new Channel("47714214", "Kri", Danya); //тестовый канал для Данечки
         Danya.addMyChannel(channel);
         for (int i = 0; i < 4; i++) {
-            channel.addSubsriber(users[i]);
+            channel.addSubscriber(users[i]);
             users[i].joinChannel(channel);
         }
         Igor.leaveChannel(channel); //Даня, прости, это для теста
 
         Channel bomb = new Channel("9876543210", "scha udalu", Igor);
         Igor.addMyChannel(bomb); //тест создания и удаления канала
-        Igor.delMyChannel(bomb);
+        Igor.delMyChannel();
 
-        Message AnyaTestMessage = new Message("27.11.2025", Anya, "Hello world!");
-        Message DanyaTestMessage = new Message("28.11.2025", Danya, "Hello world!");
-        Message ElinaTestMessage = new Message("29.11.2025", Elina, "Hello world!");
-        Message IgorTestMessage = new Message("30.11.2025", Igor, "Hello world!");
+        Message AnyaTestMessage = new Message("27.11.2025", Anya, "я хочу спать");
+        Message DanyaTestMessage = new Message("28.11.2025", Danya, "я");
+        Message ElinaTestMessage = new Message("29.11.2025", Elina, "я ничего не хочу");
+        Message IgorTestMessage = new Message("30.11.2025", Igor, "я хочу кушать");
 
-        Anya.sendMessage(AnyaTestMessage);
-        Igor.sendMessage(IgorTestMessage); //добавление по сообщению кажджому, кто пишет в чат
+        Anya.sendMessageToChat(AnyaTestMessage);
+        Igor.sendMessageToChat(IgorTestMessage); //добавление по сообщению кажджому, кто пишет в чат
         group.addMessage(AnyaTestMessage);
         group.addMessage(IgorTestMessage); //добавление сообщений в чат
-        System.out.println("Участники группы: " + group.displayMembers());
+        group.displayMembers();
         System.out.println("Всего участников: " + group.getMembersCount());
-        System.out.println("Сообщения группы: " + group.displayMessages());
+        group.displayMessages();
 
-        Danya.sendMessage(DanyaTestMessage);
+        Elina.sendMessageToChat(ElinaTestMessage);
         channel.addMessage(ElinaTestMessage); //подписчик попробовал отправить сообщение
+        Danya.sendMessageToChat(DanyaTestMessage);
         channel.addMessage(DanyaTestMessage); //админ показал кто тут главный
-        System.out.println("Подписчики канала: " + channel.displayMembers()); //осмотр подданных
-        System.out.println("Всего подписчиков: " + channel.getMembersCount());
-        System.out.println("Сообщения канала: " + channel.displayMessages());
+        channel.displaySubscribers(); //осмотр подданных
+        System.out.println("Всего подписчиков: " + channel.getSubscribersCount());
+        channel.displayMessages();
     }
 }
